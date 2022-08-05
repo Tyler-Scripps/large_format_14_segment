2498a623c8aaf0fc9fb908e90c38c5963069f279
Tyler-Scripps <tylerscripps@gmail.com>
2022-08-05 09:16:06 -0700

    added some zip librarys

--- display/display.ino
+++ display/display.ino
@@ -1,3 +1,13 @@
+#include <ESPUI.h>
+#include <dataControlsJS.h>
+#include <dataGraphJS.h>
+#include <dataIndexHTML.h>
+#include <dataNormalizeCSS.h>
+#include <dataSliderJS.h>
+#include <dataStyleCSS.h>
+#include <dataTabbedcontentJS.h>
+#include <dataZeptoJS.h>
+
 #include <FastLED.h>
 
 
