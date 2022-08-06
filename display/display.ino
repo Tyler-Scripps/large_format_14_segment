88e17ee02c90aaaa1dbb63546a0dd6a7247bc0d9
Tyler-Scripps <43222263+Tyler-Scripps@users.noreply.github.com>
2022-08-06 00:14:09 -0700

    began attempting setSegment functino

--- display/display.ino
+++ display/display.ino
@@ -10,9 +10,21 @@
 
 #include <FastLED.h>
 
+// int segments[14][2] = {{0,4},{5,9},{10,14},{15,19},{20,24},{25,29},(30,34),{35,40},{41,44},{45,49},{50,54},{55,59},{60,64},{65,69}};
+int segments[14][2] = {0,4,5,9,10,14,15,19,20,24,25,29,30,34,35,40,41,44,45,49,50,54,55,59,60,64,65,69};
+
+uint8_t globalRed = 0;
+uint8_t globalGreen = 0;
+uint8_t globalBlue = 0;
 
 
-void setSegment(char tempChar) {
+void setSegment(uint8_t segmentNum, CRGB *leds, uint8_t red, uint8_t green, uint8_t blue) {
+  for(int i = segments[segmentNum][0]; i < segments[segmentNum][1]; i++) {
+    leds[i].setRGB(red, green, blue);
+  }
+}
+
+void setDigit(char tempChar, CRGB leds) {
 
 }
 
